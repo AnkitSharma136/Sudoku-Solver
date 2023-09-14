4300e61b970caf1898095f0e240e243a26cab556
AnkitSharma136 <bt1362023@gmail.com>
2023-09-14 18:18:20 +0530

    Api updated and solve implemented

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,114 +1,93 @@
-function tableToArray(table) {
-    let result = [].reduce.call(table.rows, function (result, row) {
-        result.push([].reduce.call(row.cells, function (res, cell) {
-            res.push(parseInt(cell.children[0].value) || 0);
-            return res;
-        }, []));
-        return result;
-    }, []);
+var arr = [[], [], [], [], [], [], [], [], []]
 
-    return result;
+for (var i = 0; i < 9; i++) {
+	for (var j = 0; j < 9; j++) {
+		arr[i][j] = document.getElementById(i * 9 + j);
+
+	}
 }
 
-function checkInput(value) {
-    let regex = /^\d*$/.test(value) && parseInt(value) > 0 && parseInt(value) <= 9;
-    return !regex ? value.replace(value, '') : value
+
+var board = [[], [], [], [], [], [], [], [], []]
+
+function FillBoard(board) {
+	for (var i = 0; i < 9; i++) {
+		for (var j = 0; j < 9; j++) {
+			if (board[i][j] != 0) {
+				arr[i][j].innerText = board[i][j]
+			}
+
+			else
+				arr[i][j].innerText = ''
+		}
+	}
 }
 
-function checkColumn(board, column, value) {
-    for (let i = 0; i < board.length; i++) {
-        if (board[i][column] === value) {
-            return false;
-        }
-    }
+let GetPuzzle = document.getElementById('GetPuzzle')
+let SolvePuzzle = document.getElementById('SolvePuzzle')
 
-    return true;
+GetPuzzle.onclick = function () {
+	var xhrRequest = new XMLHttpRequest()
+	xhrRequest.onload = function () {
+		var response = JSON.parse(xhrRequest.response)
+		console.log(response)
+		board = response.board
+		FillBoard(board)
+	}
+	xhrRequest.open('get', 'https://sugoku.onrender.com/board?difficulty=easy')
+	//we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
+	xhrRequest.send()
 }
 
-function checkRow(board, row, value) {
-    for (let i = 0; i < board[row].length; i++) {
-        if (board[row][i] === value) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-function checkSquare(board, row, column, value) {
-    let squareRow = Math.floor(row / 3) * 3;
-    let squareCol = Math.floor(column / 3) * 3;
-
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[squareRow + i][squareCol + j] === value)
-                return false;
-        }
-    }
-
-    return true;
+SolvePuzzle.onclick = () => {
+	SudokuSolver(board, 0, 0, 9);
 };
 
-function checkValue(board, row, column, value) {
-    if (checkRow(board, row, value) &&
-        checkColumn(board, column, value) &&
-        checkSquare(board, row, column, value)) {
+function SudokuSolver(board, i, j, n) {
+	if(i==n){
+        FillBoard(board);
         return true;
     }
 
+    if(j==n){
+        return SudokuSolver(board,i+1,0,n);
+    }
+
+    if(board[i][j] != 0){
+        return SudokuSolver(board,i,j+1,n);
+    }
+
+    for(let num=1;num <=9 ; num++){
+        if(isValid(board,i,j,num,n)){
+            board[i][j]= num;
+            let subAns =SudokuSolver (board,i,j+1,n);
+            if(subAns){
+                return true;
+            }
+            board[i][j]=0;
+        }
+    }
     return false;
-};
-
-function findEmptyCell(board) {
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            if (board[i][j] === 0)
-                return [i, j];
-        }
-    }
-
-    return [-1, -1];
 }
 
-function solve(board) {
-    let table = document.getElementById('sudoku');
-    let emptyCell = findEmptyCell(board);
-    let row = emptyCell[0];
-    let col = emptyCell[1];
-
-    if (row === -1 || col === -1) {
-        return board;
-    }
-
-    let num = 1;
-    do {
-        if (checkValue(board, row, col, num)) {
-            board[row][col] = num;
-            let inputCell = table.rows[row].cells[col].children[0];
-            inputCell.value = num;
-            inputCell.classList.add("input-solved"); // add class css
-            solve(board);
-        }
-    } while (++num <= 9)
-
-    if (findEmptyCell(board)[0] !== -1) {
-        board[row][col] = 0;
-    }
-
-    return board;
-}
-
-function resetInputs() {
-    let inputs = document.getElementsByTagName("input");
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].type == "text") {
-            inputs[i].value = "";
-            inputs[i].classList.remove("input-solved"); // remove class css
+function isValid(board,i,j,num,n){
+    //row and col check
+    for( let x=0; x < n;x++){
+        if(board[i][x] == num || board[x][j] == num){
+            return false;
         }
     }
-}
+    //sqaure check
+    let rn = Math.sqrt(n);
+    let si =i - i % rn;
+    let sj = j - j % rn;
 
-function main(table) {
-    let board = tableToArray(table);
-    solve(board);
+    for(let x=si ;x< si + rn ;x++){
+        for(let y = sj ; y < sj ; y++){
+            if(board[x][y]==num){
+                return false;
+            }
+        }
+    }
+    return true;
 }
